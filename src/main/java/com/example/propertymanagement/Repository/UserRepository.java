34d174298a22f773c2d610b90854c758506dd2de
Dewashish Painkra <dewashishpainkra126@gmail.com>
2023-05-16 12:04:36 +0530

    Derived query method added in user repository

--- src/main/java/com/example/propertymanagement/Repository/UserRepository.java
+++ src/main/java/com/example/propertymanagement/Repository/UserRepository.java
@@ -4,6 +4,10 @@ import com.example.propertymanagement.entity.UserEntity;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 
 public interface UserRepository extends CrudRepository<UserEntity,Long> {
+
+    Optional<UserEntity> findByOwnerEmailAndPassword(String email,String password);  // find by query 
 }
